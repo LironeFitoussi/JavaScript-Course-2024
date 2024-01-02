73cad9ae67452d2fac181f1ebe9cc3d491ebddd4
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-01-02 11:12:53 +0200

    Done Lesson 8

---
+++ Section 2/8/script.js
@@ -0,0 +1,10 @@
+let js = 'amazing';
+if (js === 'amazing') {
+    alert('JavaScript is FUN!')
+};
+
+// Won't be Shown
+40 + 8 + 23 - 10
+
+// Will bw Shown
+console.log(40 + 8 + 23 - 10);
---
+++ Section 2/9/script.js
@@ -0,0 +1,5 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
