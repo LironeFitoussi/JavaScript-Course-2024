47c4739fc28b05a89ca22c56bea546970b633680
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-01-02 13:55:41 +0200

    Lesson 17 Done

---
+++ Section 2/17/Assignment/assignment.md
@@ -0,0 +1,3 @@
+LECTURE: Strings and Template Literals
+1. Recreate the 'description' variable from the last assignment, this time
+using the template literal syntax
---
+++ Section 2/17/script.js
@@ -0,0 +1,20 @@
+const firstName = "Lirone";
+const job = "Student";
+const birthYear = 1999;
+const year = 2023
+
+const lirone = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+console.log(lirone);
+
+const lironeNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(lironeNew);
+
+console.log(`Just a regular string...`);
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String with
+multiple
+lines`);
