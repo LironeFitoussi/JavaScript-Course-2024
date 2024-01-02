c996dd957f716a2fa54ad9852cdc9bb1d4305a73
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-01-02 13:11:13 +0200

    Lesson 14 Done

---
+++ Section 2/14/sript.js
@@ -0,0 +1,12 @@
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const avarageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, avarageAge);
