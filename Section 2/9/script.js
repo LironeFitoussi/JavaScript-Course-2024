c1f4449234a9c7c4a7af91320e16551b8b418437
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-01-02 11:26:11 +0200

    Lesson 9 done

--- Section 2/9/script.js
+++ Section 2/9/script.js
@@ -3,3 +3,33 @@ console.log(40 + 8 + 23 - 10);
 
 console.log('Jonas');
 console.log(23);
+
+let firstName = 'Matilda';
+// let first = 'jonas'
+// let firstNamePerson
+// let first_name_person
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Not Allowed
+// let 3years = 3; \ number at the begining
+// let jonas&matilda = 'JM' \ 
+// let new = 27 \ reserverd keyword
+// let funciton = doSomething \ reserverd keyword
+// let name = Joe \ reserverd keyword
+
+// by Convention 
+// Let Person
+
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'teacher';
+
+let job1 = 'programer'
+let job2 = 'teacher'
+
+
+console.log(myFirstJob);
