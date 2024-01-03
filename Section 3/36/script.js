a8845fcbaa9f98e1cc0f8f0d9b229c1f5f642dc7
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-01-03 22:55:43 +0200

    Update

--- Section 3/36/script.js
+++ Section 3/36/script.js
@@ -1,5 +1,13 @@
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
 function fruitProcessor(apples, oranges) {
-    console.log(apples, oranges);
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
     return juice;
 }
+
+console.log(fruitProcessor(2, 3)); //Output: Juice with 8 pieces of apples and 12 pieces of oranges.
