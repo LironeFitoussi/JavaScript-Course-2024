63b21a2c8b68c48f742b30e8d2b5fdfcb4ed523f
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-07-06 22:41:18 +0300

    Finished section

--- Section 16/268/script.js
+++ Section 16/268/script.js
@@ -6,6 +6,7 @@ const wait = function (seconds) {
   });
 }
 
+
 const createImage = function (imgPath) {
   return new Promise((resolve, reject) => {
     const img = document.createElement("img");
@@ -24,43 +25,65 @@ const createImage = function (imgPath) {
 
 let currentImg;
 
-createImage("img/img-1.jpg")
-  .then((img) => {
-    currentImg = img;
+// createImage("img/img-1.jpg")
+//   .then((img) => {
+//     currentImg = img;
+//     console.log("Image 1 loaded");
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = "none";
+//     return createImage("img/img-2.jpg");
+//   })
+//   .then(img => {
+//     currentImg = img;
+//     console.log("Image 2 loaded");
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none'
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   })
+
+// PART 1
+const loadNPause = async function () {
+  try {
+    let img = await createImage("img/img-1.jpg");
     console.log("Image 1 loaded");
-    return wait(2);
-  })
-  .then(() => {
-    currentImg.style.display = "none";
-    return createImage("img/img-2.jpg");
-  })
-  .then(img => {
-    currentImg = img;
+    await wait(2);
+    img.style.display = "none";
+    img = await createImage("img/img-2.jpg");
     console.log("Image 2 loaded");
-    return wait(2);
-  })
-  .then(() => {
-    currentImg.style.display = 'none'
-  })
-  .catch((err) => {
+    await wait(2);
+    img.style.display = "none";
+  } catch (err) {
     console.error(err);
-  })
-
-  const loadNPause = async function() {
-    try {
-      let img = await createImage("img/img-1.jpg");
-      console.log("Image 1 loaded");
-      await wait(2);
-      img.style.display = "none";
-      img = await createImage("img/img-2.jpg");
-      console.log("Image 2 loaded");
-      await wait(2);
-      img.style.display = "none";
-    } catch (err) {
-      console.error(err);
-    }
   }
+}
 
-  loadNPause();
+// loadNPause();
 
-  
+const imgArr = ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg'];
+
+const loadAll = async function (imgArr) {
+  try {
+    const imgs =
+      imgArr.map(async img => await
+        createImage(img)
+      );
+    // console.log(imgs);
+
+    const imgsEl = await Promise.all(imgs);
+    console.log(imgsEl);
+
+    imgsEl.forEach((img, i) => {
+      img.classList.add('parallel');
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+loadAll(imgArr);
