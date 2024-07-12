a55714b0e1ede12920f19408423649197e1a6c91
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-07-12 14:02:56 +0300

    137 done iife

--- Section 10/137/script.js
+++ Section 10/137/script.js
@@ -1,1 +1,27 @@
 'use strict';
+const runOnce = function () {
+  console.log('This will never run again');
+};
+
+// console.log(runOnce());
+
+// IIFE
+(function () {
+  console.log('This will never run again');
+  const isPrivate = 23;
+})();
+
+//! console.log(isPrivate); // ReferenceError
+
+// Arrow function
+(() => {
+    console.log('This will ALSO never run again');
+})();
+
+{
+    const isPrivate = 23;
+    var notPrivate = 46;
+}
+
+//! console.log(isPrivate); // ReferenceError
+//? console.log(notPrivate); // 46
