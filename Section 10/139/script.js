888308ca823deace9794fa62845b754d3c58ce0c
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-07-12 15:22:31 +0300

    dd

--- Section 10/139/script.js
+++ Section 10/139/script.js
@@ -1,1 +1,46 @@
 'use strict';
+
+// More Closures Examples
+
+// Example 1
+let f;
+
+const g = function () {
+    const a = 23;
+    f = function () {
+        console.log(a * 2);
+    };
+}
+
+const h = function () {
+    const b = 777;
+    f = function () {
+        console.log(b * 2);
+    };
+}
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+    const perGroup = n / 3;
+
+    setTimeout(function () {
+        console.log(`We are now boarding all ${n} passengers`);
+        console.log(`There are 3 groups, each with ${perGroup} passengers`);
+    }, wait * 1000);
+
+    console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000; // This will not affect the perGroup variable inside the boardPassengers function
+
+boardPassengers(180, 3);
+
