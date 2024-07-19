bee0bd7fd935e80d6b5ec2287a34c7047a25ce77
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-07-19 15:03:30 +0300

    151 Done

--- Section 11/148/script.js
+++ Section 11/148/script.js
@@ -1,8 +1,10 @@
 'use strict';
+const today = new Date();
 
 
 /////////////////////////////////////////////////
 // BANKIST APP
+
 
 // Data
 const account1 = {
@@ -61,6 +63,23 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-// LECTURES
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
 
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+        <div class="movements__date">${today.toLocaleDateString("en-US")}</div>
+        <div class="movements__value">${mov}€</div>
+      </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+}
+
+displayMovements(account1.movements);
+/////////////////////////////////////////////////
--- Section 11/151/script.js
+++ Section 11/151/script.js
@@ -1,8 +1,10 @@
 'use strict';
+const today = new Date();
+
 
 /////////////////////////////////////////////////
-/////////////////////////////////////////////////
 // BANKIST APP
+
 
 // Data
 const account1 = {
@@ -61,56 +63,47 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+        <div class="movements__date">${today.toLocaleDateString("en-US")}</div>
+        <div class="movements__value">${mov}€</div>
+      </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+}
+
+displayMovements(account1.movements);
+
+/////////////////////////////////////////////////
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-/////////////////////////////////////////////////
+const eurToUsd = 1.1;
+// const movmentsUDS = movements.map(function (mov) {
+//   // return 23
+//   return mov * eurToUsd
+// });
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
+const movmentsUDS = movements.map(mov => mov * eurToUsd);
 
-// SLICE
-/*
-console.log(arr.slice(2));
-console.log(arr.slice(2, 4));
-console.log(arr.slice(-2));
-console.log(arr.slice(-1));
-console.log(arr.slice(1, -2));
-console.log(arr.slice());
-console.log([...arr]);
-*/
+console.log(movements);
+console.log(movmentsUDS);
 
-// SPLICE
-/*
-console.log(arr.splice(2));
-arr.splice(-1);
-console.log(arr);
-arr.splice(1, 2);
-console.log(arr);
-*/
+const movmentsUDSfor = [];
+for (const mov of movements) movmentsUDSfor.push(mov * eurToUsd);
+console.log(movmentsUDSfor);
 
+const movementsDescriptions = movements.map((mov, i) => (
+  `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`
+));
 
-// REVERSE
-arr = ['a', 'b', 'c', 'd', 'e'];
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-/*
-console.log(arr2.reverse());
-console.log(arr2);
-*/
-
-// CONCAT
-
-const letters = arr.concat(arr2)
-console.log(letters);
-console.log([...arr, ...arr2]);
-
-// JOIN
-console.log(letters.join(" - "));
+console.log(movementsDescriptions);
