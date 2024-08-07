8915f20e9c10a0bd1058de9fb49953694804da3b
LironeFitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-08-07 21:10:47 +0300

    356 Done

--- Section 15/starter/script.js
+++ Section 15/starter/script.js
@@ -12,13 +12,45 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (pos) {
-        const { latitude, longitude } = pos.coords
-        // console.log(`Longtitude: ${longitude} | Latitude: ${latitude}`);
-        console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
-        
-        
-    }, function() {
-        alert('Colud not get your position')
-    })
+  navigator.geolocation.getCurrentPosition(
+    function (pos) {
+      const { latitude, longitude } = pos.coords;
+      // console.log(`Longtitude: ${longitude} | Latitude: ${latitude}`);
+      //   console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      const coords = [latitude, longitude];
+      const myHome = [32.338255, 34.8567];
+      const map = L.map('map').setView(coords, 15);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+
+      map.on('click', function (mapEvent) {
+        // console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
+    },
+    function () {
+      alert('Colud not get your position');
+    }
+  );
 }
