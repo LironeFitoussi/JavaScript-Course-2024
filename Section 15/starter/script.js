aba82e8a500a8418d5d77a6600c0150af177d68b
LironeFitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-08-07 20:37:57 +0300

    233 done

--- Section 15/starter/script.js
+++ Section 15/starter/script.js
@@ -10,3 +10,15 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (pos) {
+        const { latitude, longitude } = pos.coords
+        // console.log(`Longtitude: ${longitude} | Latitude: ${latitude}`);
+        console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+        
+        
+    }, function() {
+        alert('Colud not get your position')
+    })
+}
