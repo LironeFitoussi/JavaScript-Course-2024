53e2cfb6d156637455a33a21b7e41f554beae0fe
LironeFitoussi <lironefit@gmail.com>
2024-08-07 22:48:03 +0300

    239 Done

--- Section 15/starter/script.js
+++ Section 15/starter/script.js
@@ -11,6 +11,49 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-5)+String(Math.random()).slice(-5);
+  constructor (coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence; // steps per min
+    this.calcPace(); // Return pace in min/km
+  }
+
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain; // in meters
+    this.calcSpeed(); // Return speed in km/h
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+const run1 = new Running([39, -12], 5.2, 24, 178);
+const cycling1 = new Cycling([39, -12], 27, 95, 523);
+
+console.log(run1, cycling1);
+
+///////////////////////////////////////
+/// APPLICATION ARCHITECTURE
 class App {
   #map;
   #mapEvent;
@@ -95,5 +138,4 @@ class App {
       .openPopup();
   }
 }
-
 const app = new App();
