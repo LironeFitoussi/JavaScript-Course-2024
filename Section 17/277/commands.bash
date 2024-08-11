39208628b04392df6311d69cb97777573bc6afc4
LironeFitoussi <lironefit@gmail.com>
2024-08-11 15:28:24 +0300

    feat: Add script.js file and update commands.bash

---
+++ Section 17/277/commands.bash
@@ -0,0 +1,35 @@
+cd /home
+# cd is a command that changes the current directory to the one specified 
+
+cd ..
+# ".." is a special directory that refers to the parent directory of the current directory
+
+ls
+# ls is a command that lists the contents of the current directory
+
+clear
+# clear is a command that clears the terminal screen
+
+pwd
+# pwd is a command that prints the current working directory
+
+cd /etc
+# change directory to /etc
+
+mkdir test
+# mkdir is a command that creates a new directory
+
+touch testfile.txt
+# touch is a command that creates a new file
+
+mv testfile.txt test/
+# mv is a command that moves a file or directory to a new location
+
+cp test/testfile.txt test/testfile_copy.txt
+# cp is a command that copies a file or directory to a new location
+
+rm test/testfile_copy.txt
+# rm is a command that removes a file or directory
+
+rmdir test
+# rmdir is a command that removes an empty directory
