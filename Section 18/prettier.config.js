cd0ae11cbdaa58bbc538097eeac39cc7f348b276
LironeFitoussi <lironefit@gmail.com>
2024-08-13 22:58:10 +0300

    progression

---
+++ Section 18/prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true, // Add a semicolon at the end of every statement
+  singleQuote: true, // Use single quotes instead of double quotes
+  trailingComma: 'es5', // Add trailing commas where valid in ES5 (objects, arrays, etc.)
+  tabWidth: 2, // Set the width of a tab character to 2 spaces
+  useTabs: false, // Use spaces instead of tabs for indentation
+  bracketSpacing: true, // Add spaces between brackets in object literals
+  arrowParens: 'always', // Include parentheses around a sole arrow function parameter
+  printWidth: 80, // Wrap lines at 80 characters
+  endOfLine: 'lf', // Use line feed only (Unix-style)
+};
